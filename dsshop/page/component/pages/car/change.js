3721c4f44843d392a7c677573e799cdfe7671340
sunlulu <sll@dayu.ai>
2019-05-30 18:22:40 +0800

    车辆交换列表

--- dsshop/page/component/pages/car/change.js
+++ dsshop/page/component/pages/car/change.js
@@ -1,3 +1,4 @@
+const getUrl = require('../../../../config').getParkingCarUrl
 Page({
 	
 	/**
@@ -10,5 +11,39 @@ Page({
 		
 	},
 	
+	onShow: function (options) {
+	    
+	    this.getParkingCarList();
+
+	  },
+	  //获取停车位列表
+	  getParkingCarList(){
+	    var that = this, parkingCarList={};
+	    wx.request({
+	      url: getUrl + 'getParkingCar',
+	      data: {
+	        openid: wx.getStorageSync('openid'),
+	        verify: wx.getStorageSync('verify'),
+	        uid: wx.getStorageSync('id'),
+	      },
+	      success: function (res) {
+	        //console.log(res.data.info);
+	        if (res.data.status == 1) {
+	        	parkingCarList = res.data.info;
+	          
+	          //console.log(addressList);
+	          that.setData({
+	        	  parkingCarList: parkingCarList,
+	            
+	          }); 
+	        } else {
+	          wx.showToast({
+	            title: res.data.info,
+	            icon: 'none',
+	          })
+	        }
+	      }
+	    })
+	  },
 	
 })
