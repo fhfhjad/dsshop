b588dd8673c81d5ce61cc106b1c5df407895acd2
sunlulu <sunlulu@chinasofti.com>
2019-06-08 22:16:51 +0800

    消息列表

--- dsshop/page/component/pages/carmsg/msglist.js
+++ dsshop/page/component/pages/carmsg/msglist.js
@@ -1,66 +1,49 @@
-// page/component/pages/carmsg/msglist.js
+const getUrl = require('../../../../config').getParkingCarUrl
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data : {
 
-  },
+    },
+    onLoad : function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    onShow: function (options) {
+        this.getParkingCarList();
+    },
+    //获取停车位列表
+    getParkingCarList(){
+        var that = this, parkingCarList={};
+        wx.request({
+            url: getUrl + 'getParkingCar',
+            data: {
+                openid: wx.getStorageSync('openid'),
+                verify: wx.getStorageSync('verify'),
+                uid: wx.getStorageSync('id'),
+            },
+            success: function (res) {
+                if (res.data.status == 1) {
+                    parkingCarList = res.data.info;
+                    that.setData({
+                        parkingCarList: parkingCarList,
+                    });
+                } else {
+                    wx.showToast({
+                        title: res.data.info,
+                        icon: 'none',
+                    })
+                }
+            }
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    onAddParkingCar: function (e) {
+        wx.navigateTo({
+            url: 'add'
+        })
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
